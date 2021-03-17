d5c1a8d2ce53ba74e990ee4cdc402ca53c888d8c
Laila <l.chater@mundiapolis.ma>
2021-03-17 14:20:56 +0100

    empezar

---
+++ 0x00-linear_algebra/3-flip_me_over.py.py
@@ -0,0 +1,12 @@
+import numpy as np 
+def matrix_transpose(matrix) : 
+    new_matrix = np.array(matrix)
+    new =new_matrix.transpose()
+    return new.tolist()
+mat1 = [[1, 2], [3, 4]]
+print(mat1)
+print(matrix_transpose(mat1))
+mat2 = [[1, 2, 3, 4, 5], [6, 7, 8, 9, 10], [11, 12, 13, 14, 15],
+        [16, 17, 18, 19, 20], [21, 22, 23, 24, 25], [26, 27, 28, 29, 30]]
+print(mat2)
+print(matrix_transpose(mat2))
