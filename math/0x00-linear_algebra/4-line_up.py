0917e00b50bf42f7e582586f8f5d0e0eae1bd7d0
Laila-ch <76665291+Laila-ch@users.noreply.github.com>
2021-03-19 10:00:07 +0100

    Update 4-line_up.py

--- math/0x00-linear_algebra/4-line_up.py
+++ math/0x00-linear_algebra/4-line_up.py
@@ -1,1 +1,12 @@
-
+import numpy as np
+def add_arrays(arr1,arr2) : 
+    if len(arr1) == len(arr2) : 
+        arr1 = np.array(arr1)
+        arr2 = np.array(arr2)
+        return list (arr1+arr2)
+arr1 = [1, 2, 3, 4]
+arr2 = [5, 6, 7, 8]
+print(add_arrays(arr1, arr2))
+print(arr1)
+print(arr2)
+print(add_arrays(arr1, [1, 2, 3]))
