dba5a9fde6298fda7d110797de01bfcd4544a056
Laila-ch <76665291+Laila-ch@users.noreply.github.com>
2021-03-19 10:02:50 +0100

    Delete 0x00-linear_algebra directory

--- 0x00-linear_algebra/1-trim_me_down.py
+++
@@ -1,9 +0,0 @@
-
-matrix = [[1, 3, 9, 4, 5, 8], [2, 4, 7, 3, 4, 0], [0, 3, 4, 6, 1, 5]]
-the_middle = []
-for row in matrix : 
-    a=[row[2],row[3]]
-    the_middle.append(a)
-print("The middle columns of the matrix are: {}".format(the_middle))
-
-
