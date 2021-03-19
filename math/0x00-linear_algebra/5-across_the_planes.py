7d36b2f87b36f9914e411acd4cde359e326f7989
Laila-ch <76665291+Laila-ch@users.noreply.github.com>
2021-03-19 10:00:32 +0100

    Update 5-across_the_planes.py

--- math/0x00-linear_algebra/5-across_the_planes.py
+++ math/0x00-linear_algebra/5-across_the_planes.py
@@ -1,1 +1,19 @@
+import numpy as np
 
+def add_matrices2D(mat1, mat2) : 
+    try : 
+        mat1 = np.array(mat1)
+        mat2 = np.array(mat2)
+        mat = mat1+mat2
+        mat = mat.tolist()
+        return mat
+    except :
+        return None
+    
+    
+mat1 = [[1, 2], [3, 4]]
+mat2 = [[5, 6], [7, 8]]
+print(add_matrices2D(mat1, mat2))
+print(mat1)
+print(mat2)
+print(add_matrices2D(mat1, [[1, 2, 3], [4, 5, 6]]))
