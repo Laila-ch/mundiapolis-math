01fa81d189f5d34b2c4e74727ff6c944f11b1957
Laila-ch <76665291+Laila-ch@users.noreply.github.com>
2021-03-20 21:54:11 +0100

    Update 14-saddle_up.py

--- math/0x00-linear_algebra/14-saddle_up.py
+++ math/0x00-linear_algebra/14-saddle_up.py
@@ -1,5 +1,10 @@
 #!/usr/bin/env python3
+'''
+Saddle_up.py
+'''
 import numpy as np
 def np_matmul(mat1, mat2) : 
-    
+    '''
+    Multiplication of two matrices 
+    '''
     return np.matmul(mat1, mat2)
